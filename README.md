2f70321c1cf95cbef534eb43a27734a403f70426
unknown <monicamgarcia@gmail.com>
2022-10-07 13:29:30 -0300

    fourth commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Comision-22350-3
 TP grupo full stack python 2022 de la comision 22350 grupo 3
+Fabian Stricker y Monica Garcia
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const menu = document.querySelector(".menu");
-const openMenubtn = document.querySelector(".open-menu");
-const closeMenubtn = document.querySelector(".close-menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
 
 function toggleMenu() {
     menu.classList.toggle("menu-opened");
 }
 
-openMenubtn.addEventListener("click", toggleMenu);
-closeMenubtn.addEventListener("click", toggleMenu);
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
